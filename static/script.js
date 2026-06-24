2f12d47871509c75073afa919bf643c757e2d1a7
LostiBoy73 <julianguenther2503@gmail.com>
2026-06-24 11:27:40 +0200

    Localhost Testing ohne lokale Datenbank

--- static/script.js
+++ static/script.js
@@ -3,10 +3,7 @@
 // ==========================================
 // Für lokale Tests am PC (Lass das erstmal so stehen!)
 // Später, wenn der Pi läuft, änderst du das zu: 'https://api.robots-compliance.cc'
-const API_BASE_URL =
- window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
-   ? "http://127.0.0.1:8000"
-   : "https://api.robots-compliance.cc";
+const API_BASE_URL = "https://api.robots-compliance.cc";
 
 
 // Hilfsfunktion: Parameter aus der URL lesen (z.B. ?id=5)
